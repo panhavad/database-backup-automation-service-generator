58c664cab3f8572b3ec5a78a6aca28522583e26a
Duk Panhavad <panhavadd@gmail.com>
2020-03-15 22:50:54 +0700

    Update service-init.sh

--- service-init.sh
+++ service-init.sh
@@ -21,12 +21,10 @@ read DB_PASS
 
 echo '--> Database backup selection'
 
-echo 'Enter number of database want to backup:'
 clt='y'
 interval=0
 while [ $clt == 'y' ]
 do
-	echo $interval
 	echo 'Enter database name:'
 	read DB_NAMES[$interval]
 	echo 'Do you want to add more database?
@@ -40,9 +38,28 @@ do
 		interval=`expr $interval + 1`
 	fi
 done
-echo '#This was automatically created' > backup.sh
-echo 'd=`TZ=UTC-7 date +%d%b%y%H%M`' >> backup.sh
+mkdir /etc/dbbackup/
+#create autobackup file
+echo '#This was automatically created' > /usr/local/bin/backup-database.sh
+echo 'd=`TZ=UTC-7 date +%d%b%y%H%M`' >> /usr/local/bin/backup-database.sh
 for DB_NAME in "${DB_NAMES[@]}"
 	do
-		echo 'pg_dump --file ''"/etc/dbbackup/'$DB_NAME'_BAKUP/$d-'$DB_NAME'.sql" --compress=9 --blobs --dbname=postgresql://'$DB_USER':'$DB_PASS'@'$DB_HOST':'$DB_PORT'/'$DB_NAME >> backup.sh
+		mkdir '/etc/dbbackup/'$DB_NAME'_BAKUP'
+		echo 'pg_dump --file ''"/etc/dbbackup/'$DB_NAME'_BAKUP/$d-'$DB_NAME'.sql" --compress=9 --blobs --dbname=postgresql://'$DB_USER':'$DB_PASS'@'$DB_HOST':'$DB_PORT'/'$DB_NAME >> /usr/local/bin/backup-database.sh
 done
+
+#create service file
+echo -e "[Unit]\nDescription= Automatic database backup\n[Service]\nType=simple\nRestart=on-failure\nUser=root\nGroup=root\nExecStart=/usr/local/bin/backup-database.sh\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/auto-database-backup.service
+
+#create timer file
+echo -e "[Unit]\nDescription= Automation backup database\n[Timer]\n#Everyday at 00:00 Cambodia time\nOnCalendar=*-*-* 23:00:00\nOnActiveSec=1 hour\nPersistent=true\n[Install]\nWantedBy=timers.target" > /etc/systemd/system/auto-database-backup.timer
+
+#activate all created file
+chmod 755 /usr/local/bin/backup-database.sh
+systemctl daemon-reload
+systemctl enable auto-database-backup.service
+systemctl restart auto-database-backup.service
+systemctl enable auto-database-backup.timer
+systemctl restart auto-database-backup.timer
+
+echo "All file have been created. Enjoy your backup."
